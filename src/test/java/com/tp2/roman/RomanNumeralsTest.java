93c5bff6ff4969397febf64229af0f2a76947f20
pachugoldaracena <pgoldaracena@mail.austral.edu.ar>
2025-09-01 22:42:41 -0300

    finished RomanNumerals TDD

--- src/test/java/com/tp2/roman/RomanNumeralsTest.java
+++ src/test/java/com/tp2/roman/RomanNumeralsTest.java
@@ -5,10 +5,79 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class RomanNumeralsTest {
 
-    // TODO: Replace these lines with your tests
+    RomanNumerals num = new RomanNumerals();
+
     @Test
-    void exampleTest(){
-        assertEquals(4, 2 + 1);
+    void convertOneTest() {
+        assertEquals("I", num.convert(1));
+    }
+
+    @Test
+    void convertFiveTest() {
+        assertEquals("V", num.convert(5));
+    }
+
+    @Test
+    void convertTenTest() {
+        assertEquals("X", num.convert(10));
+    }
+
+    @Test
+    void convertFourTest() {
+        assertEquals("IV", num.convert(4));
+    }
+
+    @Test
+    void convertNineTest() {
+        assertEquals("IX", num.convert(9));
+    }
+
+    @Test
+    void convertFortyTest() {
+        assertEquals("XL", num.convert(40));
+    }
+
+    @Test
+    void convertFiftyTest() {
+        assertEquals("L", num.convert(50));
+    }
+
+    @Test
+    void convertNinetyTest() {
+        assertEquals("XC", num.convert(90));
+    }
+
+    @Test
+    void convertHundredsTest() {
+        assertEquals("C", num.convert(100));
+    }
+
+    @Test
+    void convertFourHundredTest() {
+        assertEquals("CD", num.convert(400));
+    }
+
+    @Test
+    void convertFiveHundredTest() {
+        assertEquals("D", num.convert(500));
+    }
+
+    @Test
+    void convertNineHundredTest() {
+        assertEquals("CM", num.convert(900));
+    }
+
+    @Test
+    void convertThousandTest() {
+        assertEquals("M", num.convert(1000));
+    }
+
+    @Test
+    void convertComplexTest() {
+        assertEquals("MCMXCIV", num.convert(1994));
+        assertEquals("CMXXIX", num.convert(929));
+        assertEquals("DLXII", num.convert(562));
+        assertEquals("MMMCDXCII", num.convert(3492));
     }
 
 //    Missing tests:
